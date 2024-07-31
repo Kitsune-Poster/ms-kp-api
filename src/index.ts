2bb5e7068745b92b24b59ab97c80bb208e2bc0e9
Nathan GASC <nathangasc17@gmail.com>
2024-08-01 00:52:56 +0200

    setup project

---
+++ src/index.ts
@@ -0,0 +1,49 @@
+import ConfigParser from "configparser";
+import { KSAniDB } from "./../kp-anidb-ts/dist/index";
+import express from "express";
+
+let config = new ConfigParser()
+config.read(".config")
+let client = config.get("Client", "client")
+let clientVer = config.get("Client", "client_version")
+if(client == null || clientVer == null) {
+    console.error("Client or Client Version not set in .config file. Did you forget to create a .config file? Refer to the README for more information.")
+    process.exit(1)
+}else{
+    console.log(`Client: ${client} | Client Version: ${clientVer}`)
+}
+
+const main = async () => {
+    let db = KSAniDB.builder()
+        .setClient(client)
+        .setClientVer(parseInt(clientVer))
+        .setRateLimit(1, 2000, true)
+        .setCache("./cache", 1000 * 60 * 60 * 24, true)
+        .build()
+
+    await db.init()
+
+    let app = express()
+    
+    app.get("/search/:title", async (req, res) => {
+        let animes = db.searchTitle(req.params.title)
+        res.json(animes.map(anime => anime.data()))
+    })
+
+    app.get("/details/:title", async (req, res) => {
+        let anime = db.searchTitle(req.params.title)[0]
+        let details = await anime.fetchDetails()
+        res.json(details)
+    })
+
+    app.get("/suggest/:title", async (req, res) => {
+        let suggestion = db.suggestTitle(req.params.title)
+        res.json(suggestion)
+    })
+
+    app.listen(3000, () => {
+        console.log("Server started on port 3000")
+    })
+}
+
+main()
